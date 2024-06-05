c6c5d13071adfde953fb80f3ade7dadb8a11f74e
Leksa1010 <leksa194@yahoo.com>
2024-06-05 17:47:41 +0200

    Only add option on FE is left

--- frontend/js/sport.js
+++ frontend/js/sport.js
@@ -10,6 +10,20 @@ fetch('http://localhost:8000/api/sport')
             copy.querySelector('.id').innerText = sport.id
             copy.querySelector('.name').innerText = sport.name
             copy.querySelector('.created').innerText = formatDate(sport.createdAt)
+            copy.querySelector('.remove').addEventListener('click', () => {
+                if (confirm(`Želite obrisati ${sport.name}`)) {
+                    fetch(`http://localhost:8000/api/sport/id/${sport.id}`, {
+                        method: 'DELETE',
+                    })
+                        .then(rsp => {
+                            if (rsp.status == 204) {
+                                window.location.href = './sport.html'
+                                return
+                            }
+                            alert(`Brisanje sporta nije uspelo (HTTP ${rsp.status})`)
+                        })
+                }
+            })
             table.appendChild(copy)
         })
     })
--- sport.sql
+++ sport.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `sport` /*!40100 DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `sport`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: localhost    Database: sport
@@ -39,7 +37,7 @@ CREATE TABLE `athlete` (
   KEY `fk_athlete_coach_id_idx` (`coach_id`),
   CONSTRAINT `fk_athlete_coach_id` FOREIGN KEY (`coach_id`) REFERENCES `coach` (`coach_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_athlete_sport_id` FOREIGN KEY (`sport_id`) REFERENCES `sport` (`sport_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -48,7 +46,7 @@ CREATE TABLE `athlete` (
 
 LOCK TABLES `athlete` WRITE;
 /*!40000 ALTER TABLE `athlete` DISABLE KEYS */;
-INSERT INTO `athlete` VALUES (1,'Laza','Lazić','M','2024-06-03 18:26:28',1,1,NULL,NULL),(2,'Mira','Mirić','F','2024-06-03 18:26:28',1,1,NULL,NULL),(3,'Neko','Neko','O','2024-06-03 18:26:28',1,1,NULL,NULL);
+INSERT INTO `athlete` VALUES (1,'Laza','Lazić','M','2024-06-03 18:26:28',1,1,'2024-06-05 17:26:43',NULL),(2,'Mira','Mirić','F','2024-06-03 18:26:28',1,1,NULL,NULL),(3,'Ivana','Ivanović','F','2024-06-03 18:26:28',1,1,'2024-06-05 16:32:23',NULL),(4,'Maja','Marković','F','2024-06-05 09:26:47',1,1,'2024-06-05 15:30:38',NULL),(5,'Ana','Dragić','F','2024-06-05 09:27:40',1,1,NULL,NULL);
 /*!40000 ALTER TABLE `athlete` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -64,13 +62,10 @@ CREATE TABLE `coach` (
   `name` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
   `surname` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `sport_id` int unsigned NOT NULL DEFAULT '1',
   `updated_at` datetime DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`coach_id`),
-  KEY `fk_coach_sport_id_idx` (`sport_id`),
-  CONSTRAINT `fk_coach_sport_id` FOREIGN KEY (`sport_id`) REFERENCES `sport` (`sport_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+  PRIMARY KEY (`coach_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -79,7 +74,7 @@ CREATE TABLE `coach` (
 
 LOCK TABLES `coach` WRITE;
 /*!40000 ALTER TABLE `coach` DISABLE KEYS */;
-INSERT INTO `coach` VALUES (1,'Dragomir','Marković','2024-06-03 21:13:07',1,NULL,NULL),(2,'Slavica','Rakonjac','2024-06-03 21:13:07',1,NULL,NULL),(3,'Miodrag','Nedeljković','2024-06-03 21:13:07',1,NULL,NULL),(4,'Stefan','Jovanović','2024-06-03 21:13:07',1,NULL,NULL);
+INSERT INTO `coach` VALUES (1,'Dragomir','Marković','2024-06-03 21:13:07','2024-06-05 17:35:03',NULL),(2,'Slavica','Rakonjac','2024-06-03 21:13:07',NULL,NULL),(3,'Miodrag','Nedeljković','2024-06-03 21:13:07',NULL,NULL),(4,'Stefan','Jovanović','2024-06-03 21:13:07',NULL,NULL),(5,'Jugoslav','Bulat','2024-06-05 09:29:53',NULL,NULL);
 /*!40000 ALTER TABLE `coach` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -93,9 +88,12 @@ DROP TABLE IF EXISTS `sport`;
 CREATE TABLE `sport` (
   `sport_id` int unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
   PRIMARY KEY (`sport_id`),
   UNIQUE KEY `uq_sport_name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -104,7 +102,7 @@ CREATE TABLE `sport` (
 
 LOCK TABLES `sport` WRITE;
 /*!40000 ALTER TABLE `sport` DISABLE KEYS */;
-INSERT INTO `sport` VALUES (7,'Aikido'),(2,'Fudbal'),(9,'Hokej'),(6,'Karate'),(10,'Klizanje'),(4,'Košarka'),(3,'Odbojka'),(1,'Plivanje'),(5,'Rukomet'),(8,'Vaterpolo');
+INSERT INTO `sport` VALUES (1,'Plivanje','2024-06-03 21:32:40',NULL,NULL),(2,'Fudbal','2024-06-03 21:32:40',NULL,NULL),(3,'Odbojka','2024-06-03 21:32:40',NULL,NULL),(4,'Košarka','2024-06-03 21:32:40',NULL,NULL),(5,'Rukomet','2024-06-03 21:32:40',NULL,NULL),(6,'Karate','2024-06-03 21:32:40',NULL,NULL),(7,'Aikido','2024-06-03 21:32:40',NULL,NULL),(8,'Vaterpolo','2024-06-03 21:32:40',NULL,NULL),(9,'Hokej','2024-06-03 21:32:40',NULL,NULL),(10,'Klizanje','2024-06-03 21:32:40',NULL,NULL),(11,'Rvanje','2024-06-05 17:43:38',NULL,'2024-06-05 17:45:55');
 /*!40000 ALTER TABLE `sport` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -117,4 +115,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-03 23:17:31
+-- Dump completed on 2024-06-05 17:46:46
